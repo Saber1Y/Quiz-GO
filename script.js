602680a21ca8fa5b30687c0f8035b5b0a22628f3
ayodeji <ayodejiakintobi1@gmail.com>
2023-07-26 20:08:31 +0100

    clickable functionality

--- script.js
+++ script.js
@@ -44,33 +44,74 @@ const question = [
     const questionElement = document.getElementById('question');
     const answerButton = document.getElementById('answer-buttons');
     const nextButton = document.getElementById('next-btn')
+    const scoreElement = document.getElementById('score');
+    const totalQuestionsElement = document.getElementById('total-questions');
+    let totalQuestions = question.length;
+    
+    const timeElement = document.getElementById('time');
+    let timeLeft = 0;
 
     var currentQuestionIndex = 0;
     var score = 0;
+    let timer;
+
+    function checkAnswer(isCorrect, event) {
+        if (isCorrect) {
+          score++;
+          event.target.classList.add('correct');
+        } else {
+          event.target.classList.add('incorrect');
+          answerButton.children[currentQuestion.answers.findIndex((answer) => answer.correct)].classList.add('correct');
+        }
+      
+        for (let i = 0; i < answerButton.children.length; i++) {
+          answerButton.children[i].disabled = true;
+        }
+        scoreElement.textContent = score;
+        totalQuestionsElement.textContent = totalQuestions;
+      }
 
     function startQuiz(){
         currentQuestionIndex = 0;
         score = 0;
         nextButton.innerHTML = "Next";
         showQuestion();
+        startTimer(300);
     }
 
-    function showQuestion() {
-    let currentQuestion = question[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.
-    question;
+    function startTimer(duration) {
+        let timer = duration;
+        let minutes, seconds;
+        timeLeft = timer;
+        const timerInterval = setInterval(() => {
+          minutes = Math.floor(timer / 60);
+          seconds = timer % 60;
+          timeElement.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+      
+          if (--timer < 0) {
+            clearInterval(timerInterval);
+            endQuiz();
+          }
+        }, 1000);
+      }
+
+      function showQuestion() {
+        let currentQuestion = question[currentQuestionIndex];
+        let questionNo = currentQuestionIndex + 1;
+        questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+      
+        answerButton.innerHTML = ''; // Clear existing answer buttons
+      
+        currentQuestion.answers.forEach((answer) => {
+          const button = document.createElement('button');
+          button.innerHTML = answer.text;
+          button.classList.add('btn');
+          button.addEventListener('click', (event) => checkAnswer(answer.correct, event)); // Pass 'event' here
+          answerButton.appendChild(button);
+        });
+      }
+      
+startQuiz();
 
 
-    currentQuestion.answers.forEach(answers => {
-        const button = document.createElement('button');
-        button.innerHTML = answers.text;
-        button.classList.add('btn');
-        answerButton.appendChild(button);
-    });
-    }  
-
-    startQuiz();
-
-
-alert(5);
+alert('GoodLuck!');
